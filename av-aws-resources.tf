77a9f9cb0ec05bb206fd0bb63013e649ba439555
voysest-pan <108961154+voysest-pan@users.noreply.github.com>
2022-07-15 09:29:48 -0400

    Create av-aws-resources.tf

---
+++ av-aws-resources.tf
@@ -0,0 +1,24 @@
+###########################
+# RESOURCES
+###########################
+
+resource "random_id" "seed" {
+  byte_length = 4
+}
+
+resource "azurerm_resource_group" "vnet" {
+  name     = local.name
+  location = var.region
+}
+
+module "network" {
+  source              = "Azure/network/azurerm"
+  version             = "3.1.1"
+  resource_group_name = azurerm_resource_group.vnet.name
+  vnet_name           = local.name
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.0.0/24","10.0.2.0/24","10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2","subnet3"]
+
+  depends_on = [azurerm_resource_group.vnet]
+}
